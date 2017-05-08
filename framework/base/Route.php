af12efae52e38e67aaa091b9aa520b0c376953b5
uej <534883041@qq.com>
2017-05-08 12:01:09 +0800

    测试
    测试

--- framework/base/Route.php
+++ framework/base/Route.php
@@ -1,8 +1,10 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author lxj
  */
+namespace ez\base;
 
+class Route
+{
+    
+}
